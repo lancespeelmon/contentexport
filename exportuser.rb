e7a844b89ec965b4ffc45d2f0d6cbb54f396ee6e
Duffy Gillman <duffy@rsmart.com>
2012-11-01 15:01:34 -0700

    NOJIRA

---
+++ exportuser.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'uri'
+require 'net/http'
+require 'net/https'
+require 'json'
+
+class TestUser 
+
+  def handleSakaiDoc(result)
+    puts "I'm a doc!"
+  end
+
+  def handleFile(result)
+    puts result
+  end
+
+  def handleCollection(result)
+    puts "I'm a collection!"
+  end
+
+  def handleLink(result)
+    puts "I'm a link!"
+  end
+
+  def readUser(user, items, password)
+    uri = URI("https://cole.uconline.edu/var/search/pool/auth-all.json?userid=#{user}&sortOn=_lastModified&sortOrder=desc&q=*&page=0&items=#{items}");
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      request = Net::HTTP::Get.new uri.request_uri
+      request.basic_auth('admin', password)
+      response = http.request request
+    end
+
+    json = JSON.parse(response.body)
+    results = json['results']
+
+    results.each { |result|
+
+      mimeType = result['_mimeType']
+      if (mimeType == 'x-sakai/document')
+        handleSakaiDoc result
+      elsif (mimeType == 'x-sakai/collection')
+        handleCollection result
+      elsif (mimeType == 'x-sakai/link')
+        handleLink result
+      else
+        handleFile result
+      end
+
+    }
+
+  end
+
+end
+
+if __FILE__ == $0
+  user = TestUser.new()
+  user.readUser('kdalex@ucdavis.edu', 500, ARGV[0])
+end
